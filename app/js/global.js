ad259d4e72e1b2038845bc3d92a0763d3a045a80
briana gude <briana@thesistesting.com>
2022-12-26 20:30:54 -0500

    create homepage

--- app/js/global.js
+++ app/js/global.js
@@ -1,3 +1,55 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log('helloooooo')
+  const carouselText = [
+    {text: "Welcome to Dianthe Studio!", color: "#6082bc"},
+    {text: "Pronounced dye / ANN / thee", color: "#6082bc"},
+    {text: "We work with womxn-led small businesses and startups to create custom online shopping experiences.", color: "#120F02"},
+    {text: "...because your customers should feel your presence through every interactioin with your brand.", color: "#120F02"},
+    {text: "Our website is currently under construction", color: "#120F02"},
+    {text: "We'll be accepting new clients soon <3", color: "#120F02"}
+  ]
+
+  async function typeSentence(sentence, eleRef, delay = 100) {
+    const letters = sentence.split("");
+    let i = 0;
+    while(i < letters.length) {
+      await waitForMs(delay);
+      $(eleRef).append(letters[i]);
+      i++
+    }
+    return;
+  }
+
+  async function deleteSentence(eleRef) {
+    const sentence = $(eleRef).html();
+    const letters = sentence.split("");
+    let i = 0;
+    while(letters.length > 0) {
+      await waitForMs(100);
+      letters.pop();
+      $(eleRef).html(letters.join(""));
+    }
+  }
+
+  async function carousel(carouselList, eleRef) {
+      var i = 0;
+      while(true) {
+        updateFontColor(eleRef, carouselList[i].color)
+        await typeSentence(carouselList[i].text, eleRef);
+        await waitForMs(1500);
+        await deleteSentence(eleRef);
+        await waitForMs(500);
+        i++
+        if(i >= carouselList.length) {i = 0;}
+      }
+  }
+
+  function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  function updateFontColor(eleRef, color) {
+    $(eleRef).css('color', color);
+  }
+
+  carousel(carouselText, '#sentence')
 });
