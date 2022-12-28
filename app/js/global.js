ca731852503d1ce8f83977d2ce962171b0a83726
briana gude <52926266+brianagude@users.noreply.github.com>
2022-12-28 12:07:32 -0500

    some more updates (#1)
    * some mroe updates
    
    * fix the shit you broke
    
    * bug fixes
    
    * background color
    
    * small fixes
    
    * visually center content
    
    * update text

--- app/js/global.js
+++ app/js/global.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const checkbox = document.getElementById('checkbox')
+  const mainContent = document.getElementById("mainContent");
+
+  checkbox.addEventListener('change', (e) => {
+    mainContent.classList.toggle("slide");
+  })
+
   const carouselText = [
     {text: "Welcome to Dianthe Studio!", color: "#6082bc"},
-    {text: "( pronounced dye / ANN / thee )", color: "#120F02"},
-    {text: "We work with womxn-led small businesses and startups to create custom online shopping experiences", color: "#120F02"},
+    {text: "( pronounced dye / ANN / thee )", color: "#e3b722"},
+    {text: "We work with womxn-led small businesses and startups to create custom online shopping experiences,", color: "#120F02"},
     {text: "...because your customers should feel your unique presence through every interaction with your brand.", color: "#120F02"},
-    {text: "Our website is currently under construction,", color: "#120F02"},
-    {text: "we'll be accepting new clients soon <3", color: "#82b8d4"}
+    {text: "Our website is currently under construction.", color: "#120F02"},
+    {text: "We'll be accepting new clients soon <3", color: "#82b8d4"}
   ]
 
   async function typeSentence(sentence, eleRef, delay = 100) {
@@ -31,16 +38,16 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   async function carousel(carouselList, eleRef) {
-      var i = 0;
-      while(true) {
-        updateFontColor(eleRef, carouselList[i].color)
-        await typeSentence(carouselList[i].text, eleRef);
-        await waitForMs(1500);
-        await deleteSentence(eleRef);
-        await waitForMs(500);
-        i++
-        if(i >= carouselList.length) {i = 0;}
-      }
+    var i = 0;
+    while(true) {
+      updateFontColor(eleRef, carouselList[i].color)
+      await typeSentence(carouselList[i].text, eleRef);
+      await waitForMs(1500);
+      await deleteSentence(eleRef);
+      await waitForMs(500);
+      i++
+      if(i >= carouselList.length) {i = 0;}
+    }
   }
 
   function waitForMs(ms) {
@@ -49,6 +56,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function updateFontColor(eleRef, color) {
     $(eleRef).css('color', color);
+    $('.input-cursor').css('background-color', color);
   }
 
   carousel(carouselText, '#sentence')
